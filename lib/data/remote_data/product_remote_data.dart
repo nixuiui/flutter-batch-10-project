81f2234b7dfb321b85daf19ae248282b91d16266
Niki Rahmadi Wiharto <nikirahmadiwiharto@gmail.com>
2025-12-13 20:20:28 +0700

    Implement remote data classes for authentication, products, and sales with network service integration

--- lib/data/remote_data/product_remote_data.dart
+++ lib/data/remote_data/product_remote_data.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_batch_10_project/data/remote_data/network_service/network_service.dart';
 import 'package:flutter_batch_10_project/models/product_model.dart';
 
 abstract class ProductRemoteData {
@@ -5,4 +6,25 @@ abstract class ProductRemoteData {
     int? page,
     int? size,
   });
+}
+
+class ProductRemoteDataImpl implements ProductRemoteData {
+  final NetworkService networkService;
+
+  ProductRemoteDataImpl(this.networkService);
+
+  @override
+  Future<List<Product>> getProduct({int? page, int? size}) async {
+    final response = await networkService.get(
+      url: '/api/product/list',
+      queryParameters: {
+        'page': page,
+        'size': size,
+      },
+    );
+
+    return (response.data['data']['data'] as List)
+        .map((item) => Product.fromJson(item))
+        .toList();
+  }
 }
--- lib/data/remote_data/sales_remote_data.dart
+++ lib/data/remote_data/sales_remote_data.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_batch_10_project/data/remote_data/network_service/network_service.dart';
 import 'package:flutter_batch_10_project/models/sales_model.dart';
 
 import '../../models/order_model.dart';
@@ -9,5 +10,33 @@ abstract class SalesRemoteData {
   });
 
   Future<void> postCreateSales(OrderModel data);
+}
 
+class SalesRemoteDataImpl implements SalesRemoteData {
+  final NetworkService networkService;
+
+  SalesRemoteDataImpl(this.networkService);
+
+  @override
+  Future<List<SalesInvoice>> getSalesInvoices({int? page, int? size}) async {
+    final response = await networkService.get(
+      url: '/api/sales/list',
+      queryParameters: {
+        'page': page,
+        'size': size,
+      },
+    );
+
+    return (response.data['data']['data'] as List)
+        .map((item) => SalesInvoice.fromJson(item))
+        .toList();
+  }
+
+  @override
+  Future<void> postCreateSales(OrderModel data) async {
+    await networkService.post(
+      url: '/api/sales/create',
+      data: data.toJson(),
+    );
+  }
 }
